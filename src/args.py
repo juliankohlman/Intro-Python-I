f3b818cb9e2e2bffb5fe1741e57881ff8c0b4920
Julian Kohlman <juliankohlman@gmail.com>
2019-05-13 10:38:28 -0400

    args.py completed

--- src/args.py
+++ src/args.py
@@ -1,3 +1,5 @@
+from inspect import signature
+from string import Template
 # Experiment with positional arguments, arbitrary arguments, and keyword
 # arguments.
 
@@ -6,12 +8,24 @@
 
 # YOUR CODE HERE
 
+
+def f1(a, b):
+    result = a + b
+    return result
+
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+
+
+def f2(*args):
+    # when using print() 'none' gets outputted
+    return sum(args)
+
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -21,13 +35,24 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+
+
+def f3(a, b=0):
+    if (a) and (b):
+        return a + b
+    if a:
+        return a + 1
+
+
+# https://docs.python.org/3/library/inspect.html#inspect.signature
+# sig = signature(f3)
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -42,6 +67,10 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**keywords):
+    for kw in keywords:
+        print('key:', kw, ',', 'value:', keywords[kw])
+
 
 # Should print
 # key: a, value: 12
@@ -60,4 +89,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
