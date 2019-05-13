c21ca732ef66f8ff2d3e3f9d38de33e4c1fd7cda
Julian Kohlman <juliankohlman@gmail.com>
2019-05-13 16:53:26 -0400

    Working through modules.py

--- src/comprehensions.py
+++ src/comprehensions.py
@@ -9,15 +9,18 @@ for more info regarding list comprehensions.
 """
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
+y = [x for x in range(1, 6)]
+# for x in range(1, 6):
+#     y.append(x)
 
-y = []
-
-print (y)
+print(y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
+y = [i ** 3 for i in range(0, 10)]
+# for i in range(0, 10):
+#     y.append(i ** 3)
 
 print(y)
 
@@ -26,7 +29,9 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [word.upper() for word in a]
+# for word in a:
+#     y.append(word.upper())
 
 print(y)
 
@@ -36,6 +41,7 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [i for i in x if int(i) % 2 == 0]
+
 
 print(y)
--- src/functions.py
+++ src/functions.py
@@ -2,11 +2,19 @@
 
 # YOUR CODE HERE
 
+
+def is_even(n):
+    return n % 2 == 0
+
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
 
+if (is_even(num)):
+    print('Even!')
+else:
+    print('Odd')
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
-
